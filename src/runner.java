2deb17cbeab7d47bc82a34e4f8c4c0f193ad5e57
MyriadKaelos <yateslough@students.berkeley.net>
2018-11-09 13:23:08 -0800

    Updates to documentation. It's like actually working. What I really need right now is to work out an ingame clock. How is that going to work?

--- src/runner.java
+++ src/runner.java
@@ -6,13 +6,28 @@ public class runner extends Functions {
     public runner(int width, int height, int res) {
         super(width, height, res);
     }
-
-    public static void main(String args[]) {
+/*
+I know it looks messy, but don't worry.
+This right here is where we put in the width, the height,
+and the resolution of our playing board.
+The resolution is a little bit different than the normal
+idea of resolution.
+Here, width and height give us the amount of computer pixels
+we are using, and resolution gives us the size we want our
+pixels to be.
+*/
+public static void main(String args[]) {
         runner r = new runner(300,300,10);
     }
+
+
+
     public void paintStuff(Graphics2D g2d) {
         p(10,10,Color.BLACK);
     }
+
+
+
     public void keyTyped(KeyEvent e) {
         System.out.println(e.getKeyChar() + ": Typed");
     }
